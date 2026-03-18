a713fcdcd22bdde9d3c34010822578134da87b9b
Yury Kartynnik <kartynnik@gmail.com>
2026-03-18 13:13:25 +0100

    Fix the summarization script

--- scripts/summarize.sh
+++ scripts/summarize.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-this=$(basename $(dirname "$(readlink -f "$0")"))/..
-cd ..
+this=$(basename $(dirname "$(readlink -f "$0")"))
+cd ../..
 find "$this" -name '.git' -prune -o -name '*.mhtml' -prune -o -name 'PASTED' -prune -type f -print |
   while IFS= read -r filename; do
     echo "========> FILENAME: $filename"
